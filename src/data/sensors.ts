a089d91e4ac4731431c0f5d694791295474a94dd
ManupaDev <manupadevsuite@gmail.com>
2023-06-06 03:50:40 +0530

    feat: right type for sensors

--- src/data/sensors.ts
+++ src/data/sensors.ts
@@ -1,46 +1,41 @@
 const sensors = [
-    {
-      id: 1,
-      name: "Main Light",
-      image: "",
-      state: true,
-      value:""
-    },
-    {
-      id: 2,
-      name: "Roof Light",
-      image: "",
-      state: true,
-      value:""
-    },
-    {
-      id: 3,
-      name: "Night Light",
-      image: "",
-      state: true,
-      value:""
-    },
-    {
-      id: 4,
-      name: "Ceiling Fan",
-      image: "",
-      state: true,
-      value:""
-    },
-    {
-      id: 5,
-      name: "Table Fan",
-      image: "",
-      state: true,
-      value:""
-    },
-    {
-      id: 6,
-      name: "Air Conditioner",
-      image: "",
-      state: true,
-      value:""
-    },
-  ];
+  {
+    id: 1,
+    name: "Main Light",
+    image: "",
+    value: "",
+  },
+  {
+    id: 2,
+    name: "Roof Light",
+    image: "",
+    value: "",
+  },
+  {
+    id: 3,
+    name: "Night Light",
+    image: "",
+    value: "",
+  },
+  {
+    id: 4,
+    name: "Ceiling Fan",
+    image: "",
+    value: "",
+  },
+  {
+    id: 5,
+    name: "Table Fan",
+    image: "",
+    value: "",
+  },
+  {
+    id: 6,
+    name: "Air Conditioner",
+    image: "",
 
-  export default sensors;
+    value: "",
+  },
+];
+
+export default sensors;
