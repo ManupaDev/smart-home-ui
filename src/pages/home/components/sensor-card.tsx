fa70432a5b2e6ad8dc5dc23ac2a666bca89850f0
ManupaDev <manupadevsuite@gmail.com>
2023-06-06 02:59:29 +0530

    feat: updated sensor type

--- src/pages/home/components/sensor-card.tsx
+++ src/pages/home/components/sensor-card.tsx
@@ -2,12 +2,11 @@ type Sensor = {
   id: number;
   name: string;
   image: string;
-  state: boolean;
   value: string;
 };
 
 function DeviceCard({ sensor }: { sensor: Sensor }) {
-  const { id, name, image, state } = sensor;
+  const { id, name, image } = sensor;
 
   return (
     <div className="h-64 w-64 rounded-2xl border border-black p-4">
