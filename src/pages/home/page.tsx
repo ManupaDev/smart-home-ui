09794b7cd10a0e4b9a7b9b3cd740f237fb396317
ManupaDev <manupadevsuite@gmail.com>
2023-06-06 05:02:14 +0530

    feat: best version

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -7,22 +7,62 @@ import SensorCard from "./components/sensor-card";
 import { useState } from "react";
 
 import locations from "../../data/locations";
-import devices from "../../data/devices";
 
 function HomePage() {
-  const [filteredDevices, setFilteredDevices] = useState(devices);
+  const [devices, setDevices] = useState([
+    {
+      id: 1,
+      name: "Main Light",
+      image: "",
+      state: false,
+      location: "Living Room",
+    },
+    {
+      id: 2,
+      name: "Roof Light",
+      image: "",
+      state: false,
+      location: "Bed Room",
+    },
+    {
+      id: 3,
+      name: "Night Light",
+      image: "",
+      state: false,
+      location: "Bed Room",
+    },
+    {
+      id: 4,
+      name: "Ceiling Fan",
+      image: "",
+      state: false,
+      location: "Living Room",
+    },
+    {
+      id: 5,
+      name: "Table Fan",
+      image: "",
+      state: false,
+      location: "Bed Room",
+    },
+    {
+      id: 6,
+      name: "Air Conditioner",
+      image: "",
+      state: false,
+      location: "Bed Room",
+    },
+  ]);
 
   const [selectedLocation, setSelectedLocation] = useState("All");
 
+  const filteredDevices =
+    selectedLocation === "All"
+      ? [...devices]
+      : devices.filter((device) => device.location === selectedLocation);
+
   const handleLocationSelect = (location: string) => {
-    setSelectedLocation(() => {
-      setFilteredDevices(
-        location === "All"
-          ? devices
-          : devices.filter((device) => device.location === location)
-      );
-      return location;
-    });
+    setSelectedLocation(location);
   };
 
   return (
