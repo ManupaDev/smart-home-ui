19668f5a2fbc99214256cf247e5c1fa04f49efee
ManupaDev <manupadevsuite@gmail.com>
2023-06-06 02:33:09 +0530

    feat: isolated toggle

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -3,37 +3,37 @@ const devices = [
       id: 1,
       name: "Main Light",
       image: "",
-      state: true,
+      state: false,
     },
     {
       id: 2,
       name: "Roof Light",
       image: "",
-      state: true,
+      state: false,
     },
     {
       id: 3,
       name: "Night Light",
       image: "",
-      state: true,
+      state: false,
     },
     {
       id: 4,
       name: "Ceiling Fan",
       image: "",
-      state: true,
+      state: false,
     },
     {
       id: 5,
       name: "Table Fan",
       image: "",
-      state: true,
+      state: false,
     },
     {
       id: 6,
       name: "Air Conditioner",
       image: "",
-      state: true,
+      state: false,
     },
   ];
 
--- src/pages/home/components/device-card.tsx
+++ src/pages/home/components/device-card.tsx
@@ -1,3 +1,6 @@
+import { PowerIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
 type Device = {
   id: number;
   name: string;
@@ -8,11 +11,49 @@ type Device = {
 function DeviceCard({ device }: { device: Device }) {
   const { id, name, image, state } = device;
 
+  const [power, setPower] = useState(state);
+
+  const handleClick = () => {
+    setPower(!power);
+  };
+
   return (
-    <div className="h-64 w-64 rounded-2xl border border-black p-4">
+    <div className="relative h-64 w-64 rounded-2xl border border-black p-4">
       <h1 className="text-2xl">{name}</h1>
+      <PowerButton power={power} handleClick={handleClick} />
     </div>
   );
 }
 
 export default DeviceCard;
+
+function PowerButton({
+  power,
+  handleClick,
+}: {
+  power: boolean;
+  handleClick: () => void;
+}) {
+
+  if (power) {
+    return (
+      <button
+        type="button"
+        onClick={handleClick}
+        className="absolute right-4 bottom-4 flex h-12 w-12 cursor-pointer bg-black items-center  justify-center rounded-full border border-black"
+      >
+        <PowerIcon width={36} height={36}  className="text-white"/>
+      </button>
+    );
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className="absolute right-4 bottom-4 flex h-12 w-12 cursor-pointer items-center  justify-center rounded-full border border-black"
+    >
+      <PowerIcon width={36} height={36} />
+    </button>
+  );
+}
