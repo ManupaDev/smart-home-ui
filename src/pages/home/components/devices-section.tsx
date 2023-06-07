025acd30cddd7e61719c9c3403a296eabb112dd6
ManupaDev <manupadevsuite@gmail.com>
2023-06-08 04:21:16 +0530

    feat: refactored device cards to their own section

---
+++ src/pages/home/components/devices-section.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import Device from "../../../types/device";
+import DeviceCard from "./device-card";
+import { getAllDevices } from "../../../api/api";
+
+function DevicesSection({
+  selectedLocation,
+  search,
+}: {
+  selectedLocation: string;
+  search: string;
+}) {
+  const [devices, setDevices] = useState<Device[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      try {
+        const data = await getAllDevices();
+        setDevices(data);
+      } catch (error) {
+        setIsError(true);
+      }
+    };
+    fetchDevices();
+    setIsLoading(false);
+  }, []);
+
+  const filteredDevices =
+    selectedLocation === "All"
+      ? devices.filter((device) =>
+          device.name.replace(" ", "").toLowerCase().includes(search)
+        )
+      : devices.filter((device) => {
+          return (
+            device.location === selectedLocation &&
+            device.name.replace(" ", "").toLowerCase().includes(search)
+          );
+        });
+
+  const handleDeviceUpdate = (updatedDevice: Device) => {
+    setDevices((prevDevices) => {
+      return prevDevices.map((device) =>
+        device._id === updatedDevice._id
+          ? { ...device, ...updatedDevice }
+          : device
+      );
+    });
+  };
+
+  if (isLoading) {
+    return (
+      <div className="my-4 grid grid-cols-4 gap-4  border-black">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="my-4 grid grid-cols-4 gap-4  border-black">
+        <h1>Couldn't Resolve Request</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="my-4 grid grid-cols-4 gap-4  border-black">
+      {filteredDevices.map((device, i) => (
+        <DeviceCard
+          device={device}
+          key={i}
+          handleDeviceUpdate={handleDeviceUpdate}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default DevicesSection;
--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,37 +1,17 @@
 import { SearchBar } from "./components/search-bar";
 import Header from "./components/header";
 import LocationChip from "./components/location-chip";
-import DeviceCard from "./components/device-card";
 
-import { getAllDevices } from "../../api/api";
+import { ChangeEvent, useState } from "react";
 
-import { ChangeEvent, useEffect, useState } from "react";
-
-import Device from "../../types/device";
+import DevicesSection from "./components/devices-section";
 
 function HomePage() {
   const locations = ["All", "Living Room", "Bed Room"];
 
-  const [devices, setDevices] = useState<Device[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
   const [selectedLocation, setSelectedLocation] = useState("All");
 
   const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    const fetchDevices = async () => {
-      try {
-        const data = await getAllDevices();
-        setDevices(data)
-      } catch (error) {
-        setIsError(true);
-      }
-    };
-    fetchDevices();
-    setIsLoading(false);
-  }, []);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -41,91 +21,9 @@ function HomePage() {
     setSearch("");
   };
 
-  const filteredDevices =
-    selectedLocation === "All"
-      ? devices.filter((device) =>
-          device.name.replace(" ", "").toLowerCase().includes(search)
-        )
-      : devices.filter((device) => {
-          return (
-            device.location === selectedLocation &&
-            device.name.replace(" ", "").toLowerCase().includes(search)
-          );
-        });
-
   const handleLocationSelect = (location: string) => {
     setSelectedLocation(location);
   };
-
-  const handleDeviceUpdate = (updatedDevice: Device) => {
-    setDevices((prevDevices) => {
-      return prevDevices.map((device) =>
-        device._id === updatedDevice._id
-          ? { ...device, ...updatedDevice }
-          : device
-      );
-    });
-  };
-
-  if (isLoading) {
-    return (
-      <div className="overflow-y-scroll rounded-2xl border border-black p-8">
-        <Header />
-        {/* Devices Section */}
-        <div className="my-8  border-black">
-          <h1 className="text-3xl font-semibold">Devices</h1>
-          <SearchBar
-            search={search}
-            handleChange={handleChange}
-            handleClear={handleClear}
-          />
-          <div className="my-4 flex gap-x-4 border-black">
-            {locations.map((location, i) => (
-              <LocationChip
-                key={i}
-                selectedLocation={selectedLocation}
-                handleLocationSelect={handleLocationSelect}
-                location={location}
-              />
-            ))}
-          </div>
-          <div className=" my-4 grid grid-cols-4 gap-4  border-black">
-            <h1>Loading...</h1>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className="overflow-y-scroll rounded-2xl border border-black p-8">
-        <Header />
-        {/* Devices Section */}
-        <div className="my-8  border-black">
-          <h1 className="text-3xl font-semibold">Devices</h1>
-          <SearchBar
-            search={search}
-            handleChange={handleChange}
-            handleClear={handleClear}
-          />
-          <div className="my-4 flex gap-x-4 border-black">
-            {locations.map((location, i) => (
-              <LocationChip
-                key={i}
-                selectedLocation={selectedLocation}
-                handleLocationSelect={handleLocationSelect}
-                location={location}
-              />
-            ))}
-          </div>
-          <div className=" my-4 grid grid-cols-4 gap-4  border-black">
-            <h1 className="text-red-500">Could Not Load Data</h1>
-          </div>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="overflow-y-scroll rounded-2xl border border-black p-8">
@@ -148,15 +46,7 @@ function HomePage() {
             />
           ))}
         </div>
-        <div className=" my-4 grid grid-cols-4 gap-4  border-black">
-          {filteredDevices.map((device, i) => (
-            <DeviceCard
-              device={device}
-              key={i}
-              handleDeviceUpdate={handleDeviceUpdate}
-            />
-          ))}
-        </div>
+        <DevicesSection selectedLocation={selectedLocation} search={search} />
       </div>
     </div>
   );
