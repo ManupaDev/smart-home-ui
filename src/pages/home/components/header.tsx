e4eb77787c76ed1252eff561775ab2f549f22c74
ManupaDev <manupadevsuite@gmail.com>
2023-06-08 14:07:10 +0530

    feat: updated header time

--- src/pages/home/components/header.tsx
+++ src/pages/home/components/header.tsx
@@ -1,12 +1,25 @@
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+dayjs.extend(advancedFormat)
+import { Dayjs } from "dayjs";
+
+import { useEffect, useState } from "react";
+
 function Header() {
+  const [time, setTime] = useState<Dayjs>(dayjs());
+  useEffect(() => {
+    const timer = setInterval(() => setTime(dayjs()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="bg-underlay-1 flex  items-center justify-between rounded-2xl border-black   p-4 text-white ">
       <h1 className="text-4xl ">
         Hello, <span className="font-semibold ">Manupa!</span>
       </h1>
       <div className="flex  flex-col items-end">
-        <h1 className="text-4xl font-semibold">13:57</h1>
-        <p className="mt-2">8th Thursday June 2023</p>
+        <h1 className="text-4xl font-semibold">{time.format("HH:mm")}</h1>
+        <p className="mt-2">{time.format("Do dddd MMMM YYYY")}</p>
       </div>
     </div>
   );
