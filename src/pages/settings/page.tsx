62a73f8299cca8a3656b5e13e860f8c8ea2d40d3
ManupaDev <manupadevsuite@gmail.com>
2023-06-13 11:56:55 +0530

    feat: added settings page

--- src/pages/settings/page.tsx
+++ src/pages/settings/page.tsx
@@ -1,5 +1,13 @@
 function SettingsPage() {
-    return ( <div></div> );
+  return (
+    <div className="overflow-y-scroll rounded-3xl border-black  bg-white p-8">
+      <h1 className="text-4xl font-semibold text-black">Settings</h1>
+      <div className="mt-4">
+        <h1 className="text-2xl font-semibold text-black">Account</h1>
+        
+      </div>
+    </div>
+  );
 }
 
 export default SettingsPage;
