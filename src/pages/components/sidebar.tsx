815c0f967c48e1e62b773069c4a932deca305a5f
ManupaDev <manupadevsuite@gmail.com>
2023-06-14 21:13:26 +0530

    feat: update icons

--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -1,5 +1,5 @@
 import { HomeIcon } from "@heroicons/react/24/outline";
-import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { Cog6ToothIcon,ChartPieIcon,ShieldCheckIcon } from "@heroicons/react/24/outline";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 
@@ -37,6 +37,40 @@ export function Sidebar({}) {
           );
         }}
       </NavLink>
+
+      <NavLink to={"/settings"}>
+        {({ isActive }) => {
+          return (
+            <div
+              className={clsx(
+                "flex h-16 w-16  items-center justify-center  rounded-full border-white hover:bg-smoke-300 ",
+                { "glow-1 bg-jade-500 text-black hover:bg-jade-500": isActive },
+                { "text-white": !isActive }
+              )}
+            >
+              <ChartPieIcon width={30} height={30} />
+            </div>
+          );
+        }}
+      </NavLink>
+
+      <NavLink to={"/settings"}>
+        {({ isActive }) => {
+          return (
+            <div
+              className={clsx(
+                "flex h-16 w-16  items-center justify-center  rounded-full border-white hover:bg-smoke-300 ",
+                { "glow-1 bg-jade-500 text-black hover:bg-jade-500": isActive },
+                { "text-white": !isActive }
+              )}
+            >
+              <ShieldCheckIcon width={30} height={30} />
+            </div>
+          );
+        }}
+      </NavLink>
+
+      
     </div>
   );
 }
