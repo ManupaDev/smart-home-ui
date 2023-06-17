c90c540c2b655e191a4de128d49bee60290c9aff
ManupaDev <manupadevsuite@gmail.com>
2023-06-17 17:49:49 +0530

    feat: added top level sections

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -27,8 +27,36 @@ function HomePage() {
   return (
     <div className="overflow-y-scroll rounded-3xl border-black  bg-white p-8">
       <Header />
-      <div>
-        
+      <div className=" mt-5  flex  gap-x-4">
+        <div className="relative w-3/4 border-black rounded-2xl">
+          <img
+            src="https://source.unsplash.com/yFV39g6AZ5o"
+            className="h-[300px] w-full rounded-2xl object-cover"
+            alt=""
+          />
+          <div className="absolute left-0 right-0 top-0  z-10 rounded-bl-2xl rounded-br-2xl border-black   py-2 px-4 opacity-80">
+            <div className="flex items-center  justify-between">
+              <h3 className="text-xl  font-semibold text-white">Cam 1</h3>
+              <div className="flex items-center gap-x-4">
+                <h3 className="text-2xl  font-semibold text-red-500">•</h3>
+                <h3 className="text-xl  font-semibold text-white">Live</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative border-black overflow-hidden rounded-2xl">
+          <img
+            src="https://source.unsplash.com/anapPhJFRhM"
+            className="block h-[300px] w-full hover:scale-125 hover:cursor-pointer transition-all ease-in-out duration-500 rounded-2xl object-cover"
+            alt=""
+          />
+          <div className="absolute left-0 right-0 top-0  z-10 rounded-bl-2xl rounded-br-2xl border-black   py-2 px-4 opacity-80">
+            <div className="flex items-center  justify-between">
+              <h3 className="text-xl  font-semibold text-midnight-black">Music</h3>
+              
+            </div>
+          </div>
+        </div>
       </div>
       {/* Devices Section */}
       <div className="my-5 border-black">
